4b9c8f6ae5c6f6cf08087d92e62cc76cf3a6e67c
Antoine Lemoine <antoine.lemoine@gmail.com>
2016-08-01 14:26:01 +0200

    Fix translation issue: use magento 2 __() function

--- Block/System/Config/Form/Field/Config.php
+++ Block/System/Config/Form/Field/Config.php
@@ -10,7 +10,7 @@ class Config extends \Owebia\AdvancedSettingCore\Block\System\Config\Form\Field\
 {
     protected function getFullscreenTitle(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        return $this->translate("Advanced Shipping Setting Configuration");
+        return __("Advanced Shipping Setting Configuration");
     }
 
     protected function getHelpUrl(\Magento\Framework\Data\Form\Element\AbstractElement $element)
@@ -22,14 +22,14 @@ class Config extends \Owebia\AdvancedSettingCore\Block\System\Config\Form\Field\
     {
         return parent::getToolbarContent($element)
             . <<<EOD
-<!--<a target="_blank" class="icon icon-check" href="{$this->getUrl('owebia_advancedshippingsetting/help/test')}" title="{$this->translate('Test')}"><span>{$this->translate('Test')}</span></a>-->
+<!--<a target="_blank" class="icon icon-check" href="{$this->getUrl('owebia_advancedshippingsetting/help/test')}" title="{$this->escapeHtml(__('Test'))}"><span>{$this->escapeHtml(__('Test'))}</span></a>-->
 EOD;
     }
 
     protected function getFooterContent(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         return <<<EOD
-<!--<a href="http://www.owebia.com/os2/fr/configurations" target=_blank>{$this->translate('Download configuration templates')}</a>-->
+<!--<a href="http://www.owebia.com/os2/fr/configurations" target=_blank>{$this->escapeHtml(__('Download configuration templates'))}</a>-->
 EOD;
     }
 }
