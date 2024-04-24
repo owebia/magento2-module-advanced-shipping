daa97facbe5312289435bf89bec26b8e6378e397
Antoine Lemoine <antoine.lemoine@gmail.com>
2024-04-24 10:09:20 +0000

    fix Magento2 coding standard warnings: Comment block is missing

--- Block/System/Config/Form/Field/Config.php
+++ Block/System/Config/Form/Field/Config.php
@@ -9,16 +9,28 @@ namespace Owebia\AdvancedShipping\Block\System\Config\Form\Field;
 
 class Config extends \Owebia\SharedPhpConfig\Block\System\Config\Form\Field\Config
 {
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
     protected function getFullscreenTitle(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         return __("Advanced Shipping Configuration");
     }
 
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
     protected function getHelpUrl(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         return $this->getUrl('owebia_advancedshipping/help/display');
     }
 
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
     protected function getFooterContent(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         return '';
